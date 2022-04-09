540af5d0d351ea2d0e9fbc3e0e000f8d7259b438
RandiRamadhani10 <80814841+RandiRamadhani10@users.noreply.github.com>
2022-04-09 17:00:17 +0700

    adding config color

--- src/component/navbar/index.js
+++ src/component/navbar/index.js
@@ -1,12 +1,27 @@
-import React, { Fragment } from 'react'
-import logo from '../../assets/logo_eventy.png'
-
+import React, { Fragment } from "react";
+import logo from "../../assets/logo_eventy.png";
+import "./style.css";
 const Navbar = () => {
   return (
     <Fragment>
-      <img src={logo} alt='logo' />
+      <div className="flex bg-customColor-biru justify-evenly">
+        <img src={logo} alt="logo" />
+        <div className="flex">
+          <ul id="navbar">
+            <li>
+              <a href="#home">Home</a>
+            </li>
+            <li>
+              <a href="#services">Services</a>
+            </li>
+            <li>
+              <a href="#portofolio">Portofolio</a>
+            </li>
+          </ul>
+        </div>
+      </div>
     </Fragment>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customColor: {
+          biru: "#28b7fb",
+          kuning: "#f8ce10",
+          biruTua: "#054462",
+          merah: "#dc3545",
+        },
+      },
+    },
   },
   plugins: [],
-}
+};
