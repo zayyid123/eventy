e7dbe7f22f46ebc99fb5655b3be4010e0e1ffad8
zayyid <mochamadzayyid@gmail.com>
2022-04-09 16:21:36 -0700

    add per section

--- src/pages/home-page/index.js
+++ src/pages/home-page/index.js
@@ -10,25 +10,61 @@ import { EffectCards } from "swiper";
 const Home = () => {
   return (
     <Fragment>
-        <h1 className="text-3xl font-bold underline" data-aos="fade-up" >Home</h1>
-        <Swiper
-        effect={"cards"}
-        grabCursor={true}
-        modules={[EffectCards]}
-        className="mySwiper"
-        >
-          <SwiperSlide>
-            <img src="https://d1bzsjp5fzkb7z.cloudfront.net/website/uploads/banners/71e743501d1e3691f704738a58a8dbc5.png" alt='swiper'/>
-          </SwiperSlide>
-          <SwiperSlide>Slide 2</SwiperSlide>
-          <SwiperSlide>Slide 3</SwiperSlide>
-          <SwiperSlide>Slide 4</SwiperSlide>
-          <SwiperSlide>Slide 5</SwiperSlide>
-          <SwiperSlide>Slide 6</SwiperSlide>
-          <SwiperSlide>Slide 7</SwiperSlide>
-          <SwiperSlide>Slide 8</SwiperSlide>
-          <SwiperSlide>Slide 9</SwiperSlide>
-        </Swiper>
+        <section className='home'>
+
+        </section>
+
+        <section className='tentang_eventy'>
+
+        </section>
+
+        <section className='virtual'>
+
+        </section>
+
+        <section className='solusi_eventy'>
+
+        </section>
+
+        <section className='pilihan_tipe'>
+
+        </section>
+
+        <section className='kenapa_pilih'>
+
+        </section>
+
+        <section className='testimonial'>
+
+        </section>
+
+        <section className='home_hybrid_event'>
+
+        </section>
+        
+        <section className='hybrid_event'>
+
+        </section>
+
+        <section className='detail_paket'>
+
+        </section>
+
+        <section className='Virtual_Event'></section>
+
+        <section className='Pilihan_Tampilan'></section>
+
+        <section className='Expo_Hall'></section>
+
+        <section className='Virtual_Event'></section>
+
+        <section className='Detail_Paket'></section>
+
+        <section className='Broadcast'></section>
+
+        <section className='Detail_Paket'></section>
+
+        <section className='Yuk_Bergabung'></section>
     </Fragment>
   )
 }
