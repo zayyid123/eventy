cd7af5ffe37ec438f9ae73fcf9619549acdff02b
zayyid <mochamadzayyid@gmail.com>
2022-04-16 16:15:16 -0700

    add animation to broadcast

--- src/component/home-broadcast/index.js
+++ src/component/home-broadcast/index.js
@@ -6,7 +6,7 @@ const HomeBroadcast = () => {
   return (
     <Fragment>
       <div className="bg-home-broadcast flex flex-col items-center w-full bg-customColor-biru pt-[60px]">
-        <div className="flex items-center flex-col px-[15px] md:w-[70%] ">
+        <div data-aos="fade-up" data-aos-duration="1000" className="flex items-center flex-col px-[15px] md:w-[70%] ">
           <h1 className="title-broadcast text-[46px] mb-[30px] text-white">Broadcast</h1>
           <p className="text-white text-[20px] mb-[20px] text-center">Broadcasting by Eventy adalah layanan pendukung streaming event yang membantu mengatur tampilan output event virtual Anda agar terlihat lebih menarik dan profesional.</p>
           <p className="text-white text-[20px] mb-[20px] text-center">Fasilitas live streaming bisa di design sesuai kebutuhan event Anda yang dilengkapi dengan berbagai alat dan aset pendukung yang telah disiapkan oleh Eventy.</p>
@@ -15,8 +15,8 @@ const HomeBroadcast = () => {
           Detail Paket
         </a>
         <div className="flex justify-center items-center py-[120px] flex-col">
-          <img src={img1} className="w-[70%] mb-[30px]" />
-          <img src={img2} className="w-[70%]" />
+          <img data-aos="fade-right" data-aos-duration="1000" src={img1} className="w-[70%] mb-[30px]" alt="broadcast 1" />
+          <img data-aos="fade-left" data-aos-duration="1000" src={img2} className="w-[70%]" alt="broadcast 2" />
         </div>
       </div>
     </Fragment>
