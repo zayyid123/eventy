176d82dd2247a34c1e3bf69cf6eabcd1e8c0fdd5
zayyid <mochamadzayyid@gmail.com>
2022-04-19 11:24:54 -0700

    change link sosmed

--- src/component/footer/index.js
+++ src/component/footer/index.js
@@ -12,20 +12,24 @@ const Footer = () => {
       <div className="flex flex-col items-center justify-center max-w-5xl mx-auto pt-4 pb-2">
         <div>
           <h5><span className="font-semibold text-[1rem]">Evorty Office</span> at</h5>
-          <h5>Jalan Durian Utara III, Pedalangan, Semarang, Jawa Tengah 50268</h5>
+          <h5>Jl. Poin Mas Raya, No 08, Kota Depok, Jawa Barat</h5>
           <h5>Senin - Sabtu 09.00 - 17.00 WIB</h5>
         </div>
         <div className="flex flex-wrap justify-center max-w-[400px] m-3">
           {/* ig */}
-          <div className="instagram flex rounded-[20px] items-center bg-[#5ebfff] mr-5 mb-3 pr-3">
-            <img className="w-[40px]" src={instagram} alt='instagram icon'></img>
-            <h5 className="pl-2 font-semibold text-[1rem]">Evorty.id</h5>
+          <div className="instagram rounded-[20px] bg-[#5ebfff] mr-5 mb-3 pr-3">
+            <a className="flex items-center" href='https://instagram.com/4evorty?igshid=YmMyMTA2M2Y='>
+              <img className="w-[40px]" src={instagram} alt='instagram icon'></img>
+              <h5 className="pl-2 font-semibold text-[1rem]">Evorty.id</h5>
+            </a>
           </div>
 
           {/* yt */}
-          <div className="youtube flex rounded-[20px] items-center bg-[#5ebfff] mr-5 mb-3 pr-3">
-            <img className="w-[40px]" src={youtube} alt='youtube icon'></img>
-            <h5 className="pl-2 font-semibold text-[1rem]">Evorty</h5>
+          <div className="youtube rounded-[20px] bg-[#5ebfff] mr-5 mb-3 pr-3">
+            <a className="flex items-center" href="https://youtube.com/channel/UC5imlvUNkuXxvHcctOzdW_A">
+              <img className="w-[40px]" src={youtube} alt='youtube icon'></img>
+              <h5 className="pl-2 font-semibold text-[1rem]">Evorty</h5>
+            </a>
           </div>
 
           {/* faq */}
